fdbb2392e471941440c1db61332a1fbb72a51092
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-11-04 09:47:54 -0800

    script revamp

--- src/conf-rtems.sh
+++ src/conf-rtems.sh
@@ -7,17 +7,37 @@ set -e
 TOP="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 cd "$TOP/rtems"
 
+function usage {
+	echo "Utility script to configure RTEMS"
+	echo "This will copy in the required configs for RTEMS and run ./waf configure"
+	echo "OPTIONS:"
+	echo "   -h --help               - Show this help text"
+	echo "   -t --targets <targets>  - Space separated list of target systems"
+	echo -e "\nExamples:"
+	echo " $0 # Configure for all supported targets"
+	echo " $0 -t powerpc/mvme3100 # Configure only for mvme3100"
+}
+
 while test $# -gt 0; do
     case $1 in
     -h|--help)
-        echo "A simple utility to configure RTEMS"
-        echo "This will copy in the required configs and run ./waf configure"
+		usage
         exit 1
         ;;
+	-t|--targets)
+		ALL_TARGETS="$2"
+		shift 2
+		;;
     *)
         ;;
     esac
 done
+
+if [ -z "$ALL_TARGETS" ]; then
+	ALL_TARGETS="$(cat ../configs/targets.txt | tr '\n' ' ')"
+fi
+
+echo "Targeting $ALL_TARGETS"
 
 # Save old config
 if [ -f config.ini ]; then
@@ -25,12 +45,11 @@ if [ -f config.ini ]; then
 fi
 
 # Copy in the configs
-echo "# Generated config!" > config.ini
-for c in ../configs/rtems-config.*.ini; do
-    cat $c >> config.ini
+echo "# Generated config! If you edit this, conf-rtems.sh will overwrite your changes!" > config.ini
+for target in $ALL_TARGETS; do
+	_T="$(echo $target | cut -f2 -d '/')"
+	cat ../configs/rtems-config.$_T.ini >> config.ini
 done
-
-ALL_TARGETS="$(cat ../configs/targets.txt | tr '\n' ' ')"
 
 PREFIX="$PWD/../../target/rtems"
 
