b78c8384debeef51cdb7d51724735011cffbf1ae
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-11-06 15:05:12 -0800

    Add psim run test script

---
+++ scripts/run-psim.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e 
+
+TOP="$(dirname "${BASH_SOURCE[0]}")"
+
+if [ -z "$1" ]; then
+    echo "USAGE: $0 <mybin.exe>"
+    exit 1
+fi
+
+"$TOP/../host/linux-x86_64/bin/powerpc-rtems6-run" -f "$TOP/psim-device-tree" "$1"
+
