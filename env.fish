21a885165e74f2877b0d51b41d9c3e17bf09058b
JJL772 <jeremy.lorelli.1337@gmail.com>
2025-01-07 18:19:14 -0800

    Add RTEMS_TOP variable to env scripts

--- env.fish
+++ env.fish
@@ -1,5 +1,5 @@
 # Environment configuration script for use with the fish shell
 set TOP (readlink -f (dirname (status --current-filename)))
 
+export RTEMS_TOP="$TOP"
 export PATH="$TOP/host/linux-x86_64/bin:$PATH"
-
--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@
 
 TOP="$(realpath "$(dirname ${BASH_SOURCE[0]})")"
 
+export RTEMS_TOP="$TOP"
 export PATH="$TOP/host/linux-x86_64/bin:$PATH"
 
